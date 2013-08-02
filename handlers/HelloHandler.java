b2bee54e08745b78bba617664436237e5b16af12
Michael Peterson <mpeterson4@carthage.edu>
2013-08-01 20:19:38 -0500

    Started path stuff

--- handlers/HelloHandler.java
+++ handlers/HelloHandler.java
@@ -4,11 +4,38 @@ import httpserver.*;
 
 public class HelloHandler extends HTTPHandler {
 
-  public HelloHandler(HTTPRequest request) {
-    super(request);
-  }
+	public HelloHandler(HTTPRequest request) {
+		super(request);
+		try {
+			addPath("/hello", "sayHello");
+			addPath("/hello/{String}", "sayHelloF");
+			addPath("/hello/{String}/{String}", "sayHelloFL");
 
-  public void handle() {
-    message(200, "Hello, World!");
-  }
+			addPath("/goodbye", "sayGoodbye");
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sayHello() {
+		setResponseText("Hello World");
+		setHandled(true);
+	}
+
+	public void sayHelloF() {
+		String response = "Hello " + getRequest().getPath().get(1);
+		setResponseText(response);
+		setHandled(true);
+	}
+
+	public void sayHelloFL() {
+		String response = "Hello " + getRequest().getPath().get(1) + " " + getRequest().getPath().get(2);
+		setResponseText(response);
+		setHandled(true);
+	}
+
+	public void sayGoodbye() {
+		setResponseText("Goodbye World");
+		setHandled(true);
+	}
 }
--- httpserver/HTTPHandler.java
+++ httpserver/HTTPHandler.java
@@ -1,5 +1,9 @@
 package httpserver;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 /**
  * An HTTPHandler is what all handlers used by your server descend from.
  * The only real requirement for a handler is that it has a <code>handle</code>
@@ -7,113 +11,187 @@ package httpserver;
  * HTTPResponse.
  */
 public abstract class HTTPHandler {
-  public static final String EXCEPTION_ERROR = "an exception occured while processing your request";
-  public static final String NOT_A_METHOD_ERROR = "No known method";
-  public static final String MALFORMED_INPUT_ERROR = "Malformed Input";
+	public static final String EXCEPTION_ERROR = "an exception occured while processing your request";
+	public static final String NOT_A_METHOD_ERROR = "No known method";
+	public static final String MALFORMED_INPUT_ERROR = "Malformed Input";
 
-  public static final String STATUS_GOOD = "All systems are go";
+	public static final String STATUS_GOOD = "All systems are go";
 
-  private HTTPRequest request;
-  private int responseCode;
-  private String responseType;
-  private String responseText;
-  private int responseSize;
-  private boolean handled;
+	public static HashMap<String, Method> methods = new HashMap<String, Method>();
+	private Class<? extends HTTPHandler> handler;
 
-  /**
-   * Create an HTTPHandler.
-   * @param request HTTPRequest with the browser's request.
-   */
-  public HTTPHandler(HTTPRequest request) {
-    setRequest(request);
+	private HTTPRequest request;
+	private int responseCode;
+	private String responseType;
+	private String responseText;
+	private int responseSize;
+	private boolean handled;
 
-    // default to good things
-    setResponseCode(200);
-    setResponseSize(-1);
-    setHandled(false);
-    setResponseType("text/plain");
-  }
+	/**
+	 * Create an HTTPHandler.
+	 * @param request HTTPRequest with the browser's request.
+	 */
+	public HTTPHandler(HTTPRequest request) {
+		setRequest(request);
+		handler = this.getClass();
 
-  /**
-   * Where subclasses perform their specific actions.
-   */
-  public abstract void handle();
+		// default to good things
+		setResponseCode(200);
+		setResponseSize(-1);
+		setHandled(false);
+		setResponseType("text/plain");
+	}
 
-  /**
-   * Send a simple string message with an HTTP response code back to
-   * the client. Can be used for sending all data back.
-   * @param code An HTTP response code.
-   * @param message The content of the server's response to the browser
-   */
-  public void message(int code, String message) {
-    setResponseCode(code);
-    setResponseText(message);
-    setHandled(true);
-  }
+	/**
+	 * Where subclasses perform their specific actions.
+	 * @throws HTTPException
+	 */
+	public void handle() throws HTTPException {
+		String path = getRequest().getFullPath();
 
-  /**
-   * Tell the browser there is no response data.
-   * This is done by sending over a 204 code, which means there isn't
-   * any data in the stream, but the server correctly processed the request
-   */
-  public void noContent() {
-    setResponseCode(204);
-    setResponseText("");
-    setHandled(true);
-  }
+		try {
+			Method method = methods.get(path);
 
-  /**
-   * Same as message(), but prints out an exception.
-   *
-   * @see httpserver.HTTPHandler#message
-   * @param code HTTP status code
-   * @param message the content being sent back to the browser
-   * @param t A throwable object, to be printed to the screen
-   */
-  public void error(int code, String message, Throwable t) {
-    t.printStackTrace();
-    message(code, message);
-  }
-  
-  public void setRequest(HTTPRequest request) {
-    this.request = request;
-  }
-  public HTTPRequest getRequest() {
-    return request;
-  }
+			// If the method is null, there could be dynamic text in the url
+			if (method == null) {
+				// Iterate over the keys
+				outerloop:
+					for (String key : methods.keySet()) {
+						// We need the index of '{' because it is the escape character for dynamic text
+						int index = key.indexOf('{');
 
-  public void setResponseCode(int code) {
-    responseCode = code;
-  }
-  public int getResponseCode() {
-    return responseCode;
-  }
+						// This will be manipulated based on the key
+						String newPath = "";
+						// If there is dynamic text
+						if (index != -1) {
+							// Check if the text before the '{' matches before we continue
+							if (!path.substring(0, index).equalsIgnoreCase(key.substring(0, index)))
+								index = -1;
+							else
+								newPath = path.substring(0, index-1);
+						}
+						// While we have a '{' and the path still matches what was there before
+						while (index != -1 &&
+								newPath.substring(0, index-1).equalsIgnoreCase(key.substring(0, index-1))) {
+							// Add the next part of the dynamic text to the new path
+							newPath += key.substring(index-1, key.indexOf('}', index) + 1);
 
-  public void setResponseSize(int size) {
-    responseSize = size;
-  }
-  public int getResponseSize() {
-    return responseSize;
-  }
+							// Create another string that has newPath and the rest of the regular path to test with
+							String testPath;
+							if(path.indexOf('/', index) != -1)
+								testPath = newPath + path.substring(path.indexOf('/', index));
+							else
+								testPath = newPath;
 
-  public void setHandled(boolean handled) {
-    this.handled = handled;
-  }
-  public boolean isHandled() {
-    return handled;
-  }
+							// Check for our method
+							method = methods.get(testPath);
 
-  public void setResponseText(String responseText) {
-    this.responseText = responseText;
-  }
-  public String getResponseText() {
-    return responseText;
-  }
+							// If we have found a method, invoke it and get out of here
+							if (method != null) {
+								break outerloop;
+							}
+							// Set the new index to the next index of '{'
+							index = key.indexOf('{', index + 1);
+						}
+					}
+			}
 
-  public void setResponseType(String responseType) {
-    this.responseType = responseType;
-  }
-  public String getResponseType() {
-    return responseType;
-  }
+			System.out.println("Method invoked: " + method);
+
+			method.invoke(this);
+
+		} catch (NullPointerException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
+			e.printStackTrace();
+			throw new HTTPException("Could not handle path: " + getRequest().getFullPath());
+		}
+	}
+
+	// TODO allow parameters or not?
+	public void addPath(String path, String methodName, Class<?> ... parameters) {
+		try {
+			// Add the method from the handler that added the path
+			Method method = handler.getMethod(methodName, parameters);
+			methods.put(path, method);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Send a simple string message with an HTTP response code back to
+	 * the client. Can be used for sending all data back.
+	 * @param code An HTTP response code.
+	 * @param message The content of the server's response to the browser
+	 */
+	public void message(int code, String message) {
+		setResponseCode(code);
+		setResponseText(message);
+		setHandled(true);
+	}
+
+	/**
+	 * Tell the browser there is no response data.
+	 * This is done by sending over a 204 code, which means there isn't
+	 * any data in the stream, but the server correctly processed the request
+	 */
+	public void noContent() {
+		setResponseCode(204);
+		setResponseText("");
+		setHandled(true);
+	}
+
+	/**
+	 * Same as message(), but prints out an exception.
+	 *
+	 * @see httpserver.HTTPHandler#message
+	 * @param code HTTP status code
+	 * @param message the content being sent back to the browser
+	 * @param t A throwable object, to be printed to the screen
+	 */
+	public void error(int code, String message, Throwable t) {
+		t.printStackTrace();
+		message(code, message);
+	}
+
+	public void setRequest(HTTPRequest request) {
+		this.request = request;
+	}
+	public HTTPRequest getRequest() {
+		return request;
+	}
+
+	public void setResponseCode(int code) {
+		responseCode = code;
+	}
+	public int getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseSize(int size) {
+		responseSize = size;
+	}
+	public int getResponseSize() {
+		return responseSize;
+	}
+
+	public void setHandled(boolean handled) {
+		this.handled = handled;
+	}
+	public boolean isHandled() {
+		return handled;
+	}
+
+	public void setResponseText(String responseText) {
+		this.responseText = responseText;
+	}
+	public String getResponseText() {
+		return responseText;
+	}
+
+	public void setResponseType(String responseType) {
+		this.responseType = responseType;
+	}
+	public String getResponseType() {
+		return responseType;
+	}
 }
